b10d79201ecbdaa526232bf5718a6ac6b4a20294
francisco.cadaval <francisco.cadaval@wolterskluwer.com>
2024-06-10 12:56:05 +0200

    Test - Ignore coverage on index.ts files and old tests folder.

--- jest.config.js
+++ jest.config.js
@@ -30,9 +30,11 @@ const config = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+     "node_modules",
+     "src/tests",
+     "index.ts"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
